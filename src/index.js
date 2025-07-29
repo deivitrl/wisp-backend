79da274d54a58c463e6c3a070c40dbbbec0e4605
deivitrl <deivitrl@gmail.com>
2025-07-29 04:36:53 +0000

    Corrige error de comillas invertidas en console.log

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log('Servidor corriendo en puerto ${PORT}');
 });
